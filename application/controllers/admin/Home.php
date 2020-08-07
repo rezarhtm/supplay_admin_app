18b83a155babb8bde8d986a038e5cb9e022c7c94
ez <ez@ezs-MacBook-Pro.local>
2020-08-08 05:20:18 +0700

    add middleware ke admin home

--- application/controllers/admin/Home.php
+++ application/controllers/admin/Home.php
@@ -1,10 +1,20 @@
 <?php
 
 class Home extends CI_Controller{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library(['auth']);
+
+        // Check Login & Role untuk Admin
+        $this->auth->authenticate();
+        $this->auth->isRoles("admin");
+    }
+    
     public function index(){
-        $this -> load -> view("template/admin/header");
-        $this -> load -> view("admin/home");
-        $this -> load -> view("template/admin/footer");
+        $this->load->view("template/admin/header");
+        $this->load->view("admin/home");
+        $this->load->view("template/admin/footer");
     }
 }
 
