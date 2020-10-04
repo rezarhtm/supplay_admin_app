3a3bb095dc4af0abc4938504e548fd57072a26e0
Ric <ez@ezs-MacBook-Air.local>
2020-10-05 04:00:05 +0700

    add shards

--- application/views/template/admin/header.php
+++ application/views/template/admin/header.php
@@ -15,6 +15,8 @@
 
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/shards-ui/3.0.0/css/shards.css">
 </head>
 
 <body>
--- application/views/template/vendor/header.php
+++ application/views/template/vendor/header.php
@@ -16,6 +16,7 @@
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/shards-ui/3.0.0/css/shards.css">
 	<!-- <style>
 		.dataTables_filter {
 			display: none;
